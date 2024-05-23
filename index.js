8181e151b9656a8fb2ae24293259e6f09bcd1d4a
vritta <anuhartripathi15@gmail.com>
2024-05-23 23:21:20 +0530

    added controller files and model files

--- index.js
+++ index.js
@@ -3,13 +3,17 @@ const app = express();
 require("dotenv").config();
 
 const PORT = process.env.PORT || 4000;
-
+//middleware
 app.use(express.json());
 
 const blog = require("./routes/blog");
-
+//mount
 app.use("/api/v1", blog);
 
+const connectWithDB = require("./config/database");
+connectWithDB();
+
+//start the server
 app.listen(PORT, (req, res)=>{
     console.log(`Server is started with success at PORT - ${PORT}`);
 })
