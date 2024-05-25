af025346fc943ed65699ac957fa63be2d7c3a2bf
vritta <anuhartripathi15@gmail.com>
2024-05-26 00:09:27 +0530

    created commentModel file

---
+++ models/commentModel.js
@@ -0,0 +1,20 @@
+//import mongoose
+const mongoose = require("mongoose");
+
+//route handler
+const commentSchema = new mongoose.Schema({
+    post:{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Post",    //reference to post model
+    },
+    user:{
+        type: String,
+        required: true,
+    },
+    body:{
+        type:String,
+        required: true,
+    }
+});
+//export
+module.exports = mongoose.model("comment",commentSchema);
